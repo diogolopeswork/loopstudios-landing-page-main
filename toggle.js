89d30b4b0fde1f6b277c555bd57f5476c5aeddf3
Diogo Lopes <deosl98@gmail.com>
2021-01-26 11:41:15 +0000

    commit

---
+++ toggle.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(document).on("click", "#hamburger", function(e) {
+        $("#hamburger").hide();
+        $("#c-hamburger").show();
+        $("#nav").addClass("active");
+        $("active").show();
+    }); 
+
+    $(document).on("click", "#c-hamburger", function(e) {
+        $("#hamburger").show();
+        $("#c-hamburger").hide();
+        $("#nav").removeClass("active");
+        $("active").hide();
+    });
+});
