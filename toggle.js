3fa314b6234f0c78b0261ceb205d2f6281fe6a61
Diogo Lopes <deosl98@gmail.com>
2021-02-03 12:20:00 +0000

    commit

--- toggle.js
+++ toggle.js
@@ -1,16 +1,16 @@
-$(function() {
-    $(document).on("click", "#hamburger", function(e) {
-        $("#hamburger").hide();
-        $("#c-hamburger").show();
-        $("#nav").addClass("active");
-        $("active").show();
-    }); 
+// $(function() {
+//     $(document).on("click", "#hamburger", function(e) {
+//         $("#hamburger").hide();
+//         $("#c-hamburger").show();
+//         $("#nav").addClass("active");
+//         $("active").show();
+//     }); 
 
-    $(document).on("click", "#c-hamburger", function(e) {
-        $("#hamburger").show();
-        $("#c-hamburger").hide();
-        $("#nav").removeClass("active");
-        $("active").hide();
-    });
-});
+//     $(document).on("click", "#c-hamburger", function(e) {
+//         $("#hamburger").show();
+//         $("#c-hamburger").hide();
+//         $("#nav").removeClass("active");
+//         $("active").hide();
+//     });
+// });
 
